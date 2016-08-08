e9c14569e1be0ec0030d467b7b34e80234685094
dschwartz783 <dschwartz783@gmail.com>
2016-08-08 10:58:59 -0400

    more wrapper methods

--- SwiftMine/Player/Player.swift
+++ SwiftMine/Player/Player.swift
@@ -10,4 +10,10 @@ import Foundation
 
 class Player {
     public var name: String = ""
+    public var IP: String
+    
+    init(name: String, IP: String) {
+        self.name = name
+        self.IP = IP
+    }
 }
--- SwiftMine/raknet_wrapper/RakPeerInterface.mm
+++ SwiftMine/raknet_wrapper/RakPeerInterface.mm
@@ -10,7 +10,33 @@
 #import "RakPeerInterface_objc.h"
 #import "RakPeerInterface.h"
 
+@interface RakPeerInterface()
+
+@property RakNet::RakPeerInterface *interface;
+
+@end
+
 @implementation RakPeerInterface
 
+-(instancetype)init {
+    self = [super init];
+    self.interface = RakNet::RakPeerInterface::GetInstance();
+    return self;
+}
+
+-(void)addToBanList: (NSString*) IP
+            seconds: (NSTimeInterval) seconds{
+    self.interface->AddToBanList([IP cStringUsingEncoding:NSUTF8StringEncoding], seconds);
+}
+
+-(void)addToSecurityExceptionList: (NSString*) IP {
+    self.interface->AddToSecurityExceptionList([IP cStringUsingEncoding:NSUTF8StringEncoding]);
+}
+
+-(void)advertiseSystem: (NSString*) host
+            remotePort: (unsigned short) remotePort
+                  data: (NSData*) data {
+    self.interface->AdvertiseSystem([host cStringUsingEncoding:NSUTF8StringEncoding], (unsigned short)remotePort, (const char *)data.bytes, (int)data.length);
+}
 
 @end
